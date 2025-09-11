9c98c4171599be0eab0e528f8e902f02c3c73065
heelerdeer <4paradigm2@4paradigm2deMacBook-Pro.local>
2025-09-11 15:17:40 +0800

    modify bn ln

--- batchnorm.py
+++ batchnorm.py
@@ -5,24 +5,23 @@ import numpy as np
 
 
 class BN(nn.Module):
-	def __init__(self, num_features, momentum=0.01, eps=1e-5):
-		super().__init__()
-		self.running_mean = torch.zeros(num_features)
-		self.running_var = torch.ones(num_features)
-		self.num_features = num_features
-		self.eps = eps
-		self.momentum = momentum
-		self.gamma = nn.Parameter(torch.ones(num_features))  
-		self.beta = nn.Parameter(torch.zeros(num_features))
+    def __init__(self, num_features, momentum=0.01, eps=1e-5):
+        super().__init__()
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.gamma = nn.Parameter(torch.ones(num_features))
+        self.beta = nn.Parameter(torch.zeros(num_features))
 
-	def forward(self, x):
-		if self.training:
-			#[B,D]
-			x_mean = x.mean(dim=0, keepdim=True)
-			x_var = x.var(dim=0, keepdim=True, unbiased=False)
-			self.running_mean = (1 - self.momentum) * x_mean + self.momentum * self.running_mean
-			self.running_var = (1 - self.momentum) * x_var + self.momentum * self.running_var
-			x_normalized = (x - x_mean) / np.sqrt(x_var+self.eps)
-		else:
-			x_normalized = (x - self.running_mean) / np.sqrt(self.running_var + self.eps)
-		return self.gamma * x_normalized + self.beta
+    def forward(self, x):
+        if self.training:
+            x_mean = x.mean(dim=0, keepdim=True)
+            x_var = x.var(dim=0, keepdim=True, unbiased=False)
+            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * x_mean
+            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * x_var
+            x_normalized = (x - x_mean) / torch.sqrt(x_var + self.eps)
+        else:
+            x_normalized = (x - self.running_mean) / torch.sqrt(self.running_var + self.eps)
+        return self.gamma * x_normalized + self.beta
--- layernorm.py
+++ layernorm.py
@@ -3,13 +3,14 @@ from torch import nn
 import numpy as np
 
 class LN(nn.Module):
-	def __init__(self, features, eps=1e-5):
-		super().__init__()
-		self.eps = eps
-		# 对应LN中需要更新的beta和gamma，采用pytorch文档中的初始化值
-		self.gamma = nn.Parameter(torch.ones(features))  # 缩放参数
-		self.beta = nn.Parameter(torch.zeros(features))  # 偏移参数
-	def forward(self, x):
-		x_mean = x.mean(dim=-1, keepdim=True)
-		x_var = x.var(dim=-1, keepdim=True, unbiased=False)
-		return self.gamma * (x - x_mean) / np.sqrt(x_var+self.eps) + self.beta
+    def __init__(self, features, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+
+    def forward(self, x):
+        x_mean = x.mean(dim=-1, keepdim=True)
+        x_var = x.var(dim=-1, keepdim=True, unbiased=False)
+        x_normalized = (x - x_mean) / torch.sqrt(x_var + self.eps)
+        return self.gamma * x_normalized + self.beta
